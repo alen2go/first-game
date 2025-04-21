6aeb63d2988136a4df5b3e1e1ce43e56c44b2daf
alen2go <odisho_alen@hotmail.de>
2025-04-21 22:31:03 +0200

    finished the godot-tutorial

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-
 func _on_body_entered(body: Node2D) -> void:
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -7,7 +7,6 @@ var direction = 1
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
